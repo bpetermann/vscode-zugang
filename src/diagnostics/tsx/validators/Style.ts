f519175aa8cea2d0084e3c1b0590f1a3728859c8
bpetermann <benjamin.petermann@gmx.at>
2025-01-21 08:29:38 +0100

    refactor: utilize style helper

--- src/diagnostics/tsx/validators/Style.ts
+++ src/diagnostics/tsx/validators/Style.ts
@@ -1,8 +1,8 @@
 import { SourceLocation } from '@babel/types';
 import { isRatioOk } from 'hue-check';
 import { DiagnosticSeverity } from 'vscode';
-import { EM, PROBLEMATIC_FONTS, PX, REM } from '../../utils/constants';
 import { messages } from '../../utils/messages';
+import { Style } from '../../utils/Style';
 import { Diagnostic } from '../Diagnostic';
 import { TSXElement } from '../Element';
 import { Visitor } from './Validator';
@@ -45,7 +45,7 @@ export class StyleValidator implements Visitor<Diagnostic[]> {
   private checkFontSize(): Diagnostic | undefined {
     const { fontSize } = this.style;
 
-    if (fontSize && !this.isFontSizeSufficent(fontSize.toString())) {
+    if (fontSize && !Style.isFontSizeSufficent(fontSize.toString())) {
       return new Diagnostic(messages.style.font, this.loc);
     }
   }
@@ -53,7 +53,7 @@ export class StyleValidator implements Visitor<Diagnostic[]> {
   private checkFontFamily(): Diagnostic | undefined {
     const { fontFamily } = this.style;
 
-    if (this.isFontProblematic(fontFamily?.toString() || '')) {
+    if (Style.isFontProblematic(fontFamily?.toString() || '')) {
       return new Diagnostic(
         messages.style.family + fontFamily,
         this.loc,
@@ -70,62 +70,9 @@ export class StyleValidator implements Visitor<Diagnostic[]> {
       lineHeight &&
       typeof lineHeight !== 'boolean' &&
       typeof fontSize !== 'boolean' &&
-      !this.isLineHeightSufficient(lineHeight, fontSize)
+      !Style.isLineHeightSufficient(lineHeight, fontSize)
     ) {
       return new Diagnostic(messages.style.height, this.loc);
-    }
-  }
-
-  private isFontProblematic(fontFamily: string): boolean {
-    const problematicFontsLower = PROBLEMATIC_FONTS.map((font) =>
-      font.toLowerCase()
-    );
-    return fontFamily
-      .toLowerCase()
-      .split(',')
-      .some((font) =>
-        problematicFontsLower.includes(font.trim().replace(/["']/g, ''))
-      );
-  }
-
-  private isFontSizeSufficent(
-    size: string = '',
-    minPx: number = 9,
-    minEm: number = 0.563
-  ): boolean {
-    return (
-      (size && !isNaN(+size) && +size >= minPx) ||
-      (size?.endsWith(PX) && +size.split(PX)[0] >= minPx) ||
-      (size?.endsWith(EM) && +size.split(EM)[0] >= minEm) ||
-      (size?.endsWith(REM) && +size.split(REM)[0] >= minEm)
-    );
-  }
-
-  private isLineHeightSufficient(
-    lineHeight: string | number,
-    fontSize: string | number
-  ): boolean {
-    const fontSizeValue = this.parseValue(fontSize, 16);
-    const lineHeightValue = this.parseValue(
-      lineHeight.toString(),
-      fontSizeValue
-    );
-
-    return lineHeightValue >= fontSizeValue * 1.5;
-  }
-
-  private parseValue(value: string | number, reference: number = 0): number {
-    switch (true) {
-      case typeof value === 'number':
-        return value;
-      case typeof value === 'string' && value.endsWith('px'):
-        return parseFloat(value);
-      case typeof value === 'string' && value.endsWith('em'):
-        return parseFloat(value) * reference;
-      case typeof value === 'string' && value.endsWith('%'):
-        return (parseFloat(value) / 100) * reference;
-      default:
-        return parseFloat(value) * reference || parseFloat(value);
     }
   }
 }
